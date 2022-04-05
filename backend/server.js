1516dd24214511782120f3fa6393ff72fda5dbdf
ryanjgani <ryanscar9@gmail.com>
2022-04-06 03:45:53 +0800

    CORS

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
     cors({
         credentials: true,
         // origin: "https://travel-app-mern.herokuapp.com/",
-        origin: "http://localhost:3000/",
+        origin: "http://localhost:3000",
     })
 );
 
